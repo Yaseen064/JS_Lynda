a944f28d25ae91d9609ba9ccde5da5d8ab951142
Mohammed Yaseen <31075484+Yaseen064@users.noreply.github.com>
2018-07-05 19:43:32 +0530

    Using_JSONP_Approach
    Using_JSONP_Approach

--- finished/json/myscript.js
+++ finished/json/myscript.js
@@ -1,22 +1,18 @@
-$(document).ready(function() {
+function dataHandler(info) {
+	var output='';
+	for (var i = 0; i <= info.links.length-1; i++) {
+		for (key in info.links[i]) {
+			if (info.links[i].hasOwnProperty(key)) {
+				output += '<li>' + 
+				'<a href = "' + info.links[i][key] +
+				'">' + key + '</a>';
+				'</li>';
+	    }   
+		}
+	}
 
-	$.getJSON('data.json', function(info){
+	var update = document.getElementById('links');
+	update.innerHTML = output;
 
-			var output='';
-			for (var i = 0; i <= info.links.length-1; i++) {
-				for (key in info.links[i]) {
-					if (info.links[i].hasOwnProperty(key)) {
-						output += '<li>' + 
-						'<a href = "' + info.links[i][key] +
-						'">' + key + '</a>';
-						'</li>';
-			    }   
-				}
-			}
-			
-			var update = document.getElementById('links');
-			update.innerHTML = output;
+}
 
-	}); //getJSON
-
-}); // ready
